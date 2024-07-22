d6c274403cab15df2a50272391f5e1f2c28b7231
oroszlanolo <oroszlanolo@index.hu>
2024-07-22 23:28:32 +0200

    test fixes

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [HomeComponent]
+    imports: [
+      HttpClientTestingModule,
+      HomeComponent]
 })
     .compileComponents();
 
