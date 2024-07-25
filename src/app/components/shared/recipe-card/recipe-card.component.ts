a0564489ba8d7544782ab7deefe0fe7f6341dfa7
oroszlanolo <oroszlanolo@index.hu>
2024-07-25 21:32:11 +0200

    router link fix

--- src/app/components/shared/recipe-card/recipe-card.component.ts
+++ src/app/components/shared/recipe-card/recipe-card.component.ts
@@ -1,12 +1,13 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { Recipe } from 'src/recipe';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css',
   host: {
